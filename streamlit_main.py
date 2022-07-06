d169584f4ea20e1b9ab97b1df56795275d584c59
mickolka <mikalai.rahachou@gmail.com>
2022-07-07 00:34:08 +0200

    Update Streamlit interface

--- streamlit_main.py
+++ streamlit_main.py
@@ -5,6 +5,8 @@ import functions
 
 st.set_page_config(page_title="Text comparison", page_icon="🧐", layout="wide")
 
+st.title("Text comparison")
+
 with st.spinner("Loading"):
     nltk.download("popular")
 
@@ -20,17 +22,21 @@ with st.form("text_form"):
 
 if submitted:
     if text1 == "" or text2 == "":
-        st.write("Please enter both texts")
+        st.error("Please enter both texts")
         st.stop()
     with st.spinner("Calculating the metrics"):
         stats1 = functions.get_stats(text1)
         stats2 = functions.get_stats(text2)
+    st.write("##### Metrics for text 1 and text 2:")
+    st.write(functions.get_table(stats1, stats2))
+    with st.spinner("Calculating the bootstrapped confidence interval for text 1"):
         text1_ci_left = functions.bootstrapped_ci(text1, q=0.025, n=1000)
         text1_ci_right = functions.bootstrapped_ci(text1, q=0.975, n=1000)
+    st.write("##### Bootstrapped 95% confidence interval for text 1:")
+    st.write(functions.get_table(text1_ci_left, text1_ci_right))
+    with st.spinner("Calculating the bootstrapped confidence interval for text 2"):
         text2_ci_left = functions.bootstrapped_ci(text2, q=0.025, n=1000)
         text2_ci_right = functions.bootstrapped_ci(text2, q=0.975, n=1000)
-
-    st.write(functions.get_table(stats1, stats2))
-    st.write(functions.get_table(text1_ci_left, text1_ci_right))
+    st.write("##### Bootstrapped 95% confidence interval for text 2:")
     st.write(functions.get_table(text2_ci_left, text2_ci_right))
     st.caption("p/s — per sentence")
